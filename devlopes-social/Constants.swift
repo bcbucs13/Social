147cd6a5b97736a72c4a6e2a7993b5a9be1b4bce
Bradley Carter <bcbucs13@yahoo.com>
2017-01-08 13:29:29 -0500

    Sign in ui custom classes complete

---
+++ devlopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devlopes-social
+//
+//  Created by Marissa Dietz on 1/8/17.
+//  Copyright © 2017 Carter Apps. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0/255.0
