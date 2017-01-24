1dbd714ccb24e15c93f83949cf99d5f239742928
Bradley Carter <bcbucs13@yahoo.com>
2017-01-23 23:32:41 -0500

    Feed UI part 1 complete

--- devlopes-social/AdvancedImageView.swift
+++ devlopes-social/AdvancedImageView.swift
@@ -1,23 +1,35 @@
 //
-//  AdvancedView.swift
+//  AdvancedImageView.swift
 //  devlopes-social
 //
-//  Created by Marissa Dietz on 1/8/17.
+//  Created by Marissa Dietz on 1/19/17.
 //  Copyright © 2017 Carter Apps. All rights reserved.
 //
 
 import UIKit
 
-class AdvancedView: UIView {
+class AdvancedImageView: UIImageView {
 
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green:SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-        
+
     }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+    }
+    
 
 }
