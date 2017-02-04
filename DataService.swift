7b5291ec68ce01021924760b437a03e7f8fe7907
Bradley Carter <bcbucs13@yahoo.com>
2017-02-04 10:37:40 -0500

    Completed creating Firebase DB Users

---
+++ DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  devlopes-social
+//
+//  Created by Marissa Dietz on 1/30/17.
+//  Copyright © 2017 Carter Apps. All rights reserved.
+//
+
+import Foundation
---
+++ devlopes-social/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  devlopes-social
+//
+//  Created by Marissa Dietz on 1/23/17.
+//  Copyright © 2017 Carter Apps. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
